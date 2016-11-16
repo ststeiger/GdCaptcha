a5be00dfecf415a60af521e7b549fc82e4dc82c3
ststeiger <quandary82@hailmail.net>
2016-11-16 18:34:18 +0100

    Refactor

--- GdTest/Captcha3D.cs
+++ GdTest/Captcha3D.cs
@@ -31,30 +31,44 @@ namespace GdTest
         public static byte[] Generate()
         {
             string captchaText = Markov.generateCaptchaTextMarkovClean(5);
-            //captchaText = "A quick brown fox jumped over the lazy dog.";
             captchaText = "The quick brown fox jumps over the  lazy dog";
-            // captchaText = "test123";
+            captchaText = "test123";
             
             byte[] imageBytes = Generate(captchaText, System.Drawing.Imaging.ImageFormat.Png);
             System.IO.File.WriteAllBytes(FileHelper.MapProjectPath("Img/mesh.png"), imageBytes);
             
             return imageBytes;
-        }
+        } // End Function Generate 
 
 
         public static byte[] Generate(string captchaText, System.Drawing.Imaging.ImageFormat format)
         {
             byte[] imageBytes = null;
+            double[][] coord = null;
+            int image2d_x = 0;
+            int image2d_y = 0;
+            
 
+            double bevel = 3;
             float fontSize = 20f;
             string fontFamily = "Arial";
             fontFamily = "Algerian";
             
 
-            double bevel = 3;
-            double[][] coord = null;
-            int image2d_x = 0;
-            int image2d_y = 0;
+
+            // Calculate projection matrix
+            double[] T = MathHelpers.cameraTransform(
+                new double[] { 0, -200, 250 },
+                // new double[] { Helpers.rand(-90, 90), -200, Helpers.rand(150, 250) }, 
+                new double[] { 0, 0, 0 }
+            );
+
+
+            T = MathHelpers.matrixProduct(
+                T,
+                // MathHelpers.viewingTransform(60, 300, 3000)
+                MathHelpers.viewingTransform(15, 30, 3000)
+            );
 
 
             string fontFile = FileHelper.MapProjectPath("Img/3DCaptcha.ttf");
@@ -69,7 +83,7 @@ namespace GdTest
                     using (System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(bmp))
                     {
                         System.Drawing.SizeF size = g.MeasureString(captchaText, font, new System.Drawing.PointF(10, 10), System.Drawing.StringFormat.GenericTypographic);
-                        image2d_x = (int)(size.Width * 1.15) + 3;
+                        image2d_x = (int)(size.Width * 1.1) + 5;
                         image2d_y = (int)(size.Height);
                     } // End Using g 
 
@@ -84,19 +98,6 @@ namespace GdTest
                         g.DrawString(captchaText, font, System.Drawing.Brushes.White, new System.Drawing.PointF(1, -1));
                         image2d.Save(FileHelper.MapProjectPath("Img/ftstring.png"), System.Drawing.Imaging.ImageFormat.Png);
 
-                        // Calculate projection matrix
-                        double[] T = MathHelpers.cameraTransform(
-                                         new double[] { 0, -200, 250 },
-                            // new double[] { Helpers.rand(-90, 90), -200, Helpers.rand(150, 250) },
-                                         new double[] { 0, 0, 0 }
-                                     );
-
-
-                        T = MathHelpers.matrixProduct(
-                            T,
-                            MathHelpers.viewingTransform(60, 300, 3000)
-                            //Helpers.viewingTransform(90, 300, 900000000000)
-                        );
 
                         coord = new double[image2d_x * image2d_y][]; // { image2d_x * image2d_y };
                         // Calculate coordinates
@@ -106,13 +107,10 @@ namespace GdTest
                         {
                             for (int x = 0; x < image2d_x; x++)
                             {
-
-                                // calculate x1, y1, x2, y2
+                                // Calculate x1, y1, x2, y2
                                 double xc = x - image2d_x / 2.0;
-                                //double zc = y - dblimage2d_y / 2.0;
                                 double zc = y - image2d_y / 2.0;
 
-                                //yc = -(imagecolorat(image2d, x, y) & 0xff) / 256 * bevel;
                                 double yc = -(image2d.GetPixel(x, y).ToArgb() & 0xff) / 256.0 * bevel;
                                 double[] xyz = new double[] { xc, yc, zc, 1 };
                                 xyz = MathHelpers.vectorProduct(xyz, T);
@@ -137,23 +135,16 @@ namespace GdTest
             int image3d_y = image3d_x * 9 / 16;
 
             image3d_x = 256 * 4;
-            image3d_y = (int)(image3d_x * 0.5);
+            image3d_y = (int)(image3d_x * 0.05);
 
 
-
-            // image3d = imagecreatetruecolor(image3d_x, image3d_y);
             using (System.Drawing.Bitmap image3d = new System.Drawing.Bitmap(image3d_x, image3d_y))
             {
-                //var bgcolor = image3d.ColorAllocate(255, 255, 255);
-                //var fgcolor = image3d.ColorAllocate(0, 0, 255);
-
-                // imageantialias(image3d, true);
-                //image3d.SetAntiAliased();
-
-                // imagefill(image3d, 0, 0, bgcolor);
-
                 using (System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(image3d))
                 {
+                    //g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.Default;
+                    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
                     g.Clear(System.Drawing.Color.White);
                     int count = 0;
                     double scale = 1.75 - image2d_x / 400.0;
@@ -172,7 +163,7 @@ namespace GdTest
                                 double x1 = coord[count][0] * scale + image3d_x / 2.0;
                                 double y1 = coord[count][1] * scale + image3d_y / 2.0;
 
-                                // imageline(image3d, x0, y0, x1, y1, fgcolor);
+                                //using (System.Drawing.Pen pen = new System.Drawing.Pen(System.Drawing.Color.Blue, 0.25f)) { g.DrawLine(pen, (int)x0, (int)y0, (int)x1, (int)y1); }
                                 g.DrawLine(System.Drawing.Pens.Blue, (int)x0, (int)y0, (int)x1, (int)y1);
                             } // End if (x > 0) 
 
